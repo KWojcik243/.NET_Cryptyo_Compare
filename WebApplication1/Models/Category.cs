3482c531171055c7710de2bc3577d8afa9d8498b
KWojcik243 <KWojcik243@users.noreply.github.com>
2022-05-17 21:11:21 +0200

    First inicialization

---
+++ WebApplication1/Models/Category.cs
@@ -0,0 +1,7 @@
+﻿namespace WebApplication1.Models;
+
+public class Category
+{
+    public ulong Id { get; set; }
+    public string Name { get; set; }  //Property
+}
---
+++ WebApplication1/SWLDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+
+namespace WebApplication1;
+
+public class SWLDbContext : DbContext
+{
+    public DbSet<Category> Categories { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlite("Data Source=db.sqlite");
+}
